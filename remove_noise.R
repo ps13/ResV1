1d7ff3ac9fa4220b51ee0ad87d5d8468ffcbd7d9
harris11 <ckyriako@stevens.edu>
2014-01-24 19:01:48 -0500

    improving the noise deletion part

--- remove_noise.R
+++ remove_noise.R
@@ -1,5 +1,63 @@
-# remove noise
-# delete duplicated tweets and automatically created from games, apps etc.
+###########################################################################################################################
+# delete duplicated tweets from their tweet id
+###########################################################################################################################
 
-length(unique(twtest1$tweet))
-length(unique(twtest1$tweetid))
+# find which rows are duplicated
+temp <- which(duplicated(twtest1$tweetid))
+twtest2 <- twtest1[-temp,]
+# remove noise
+
+
+
+###########################################################################################################################
+# delete duplicated tweets and automatically created from games, apps etc
+###########################################################################################################################
+#save the ones that have been retweeted
+
+
+# find which rows are duplicated
+length(which(duplicated(twtest1$tweet)))
+
+
+
+###########################################################################################################################
+# find most frequently tweeted text
+###########################################################################################################################
+head(summary(as.factor(twtest1$tweet)))
+summary(as.factor(twtest1$tweet[1:100]))
+
+
+
+
+# delet all tweets that occured more than 10 
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################################################################################################
+# find which rows contain the word "game"
+###########################################################################################################################
+
+game <- c()
+kw <- c("game", "games", "play")
+for (i in seq(1, length(kw))){
+  game <- c(game, grep(kw[i], twtest1$tweet,ignore.case = TRUE))
+}
+
+#get only the unique values in the list
+game <- unique(game)
+length(game)
+length(which(grep("game", twtest1$tweet,ignore.case = TRUE)))
+
+
+
+
+
