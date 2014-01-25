5751acbd91222bb836040e50d8c56b3d2d7bc9dd
ps <pozturk@stevens.edu>
2014-01-24 21:49:29 -0500

    add f-1 score

--- classifier2.py
+++ classifier2.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.multiclass import OneVsRestClassifier
 import csv, random
 import nltk
+from sklearn.metrics import f1_score,classification_report, confusion_matrix
 
 username="pinarozturk"
 fp = open( '/Users/'+username+'/Dropbox/researthon/sentiment/android.csv', 'rb' )
@@ -22,16 +23,24 @@ y_train=[]
 X_test=[]
 Y_test=[]
 #print 0.8*len(tweets)
-num_train = int(0.8 * len(tweets))
+num_train = int(1 * len(tweets))
 for i in range(0,num_train):
     X_train.append(tweets[i][0])
     y_train.append(tweets[i][1])
     
 X_train=np.array(X_train)
 
-for i in range(num_train, len(tweets)):
-    X_test.append(tweets[i][0])
-    Y_test.append(tweets[i][1])
+testfp = open( '/Users/'+username+'/Dropbox/researthon/sentiment/ios.csv', 'rb' )
+reader = csv.reader( testfp, delimiter=',', quotechar='"', escapechar='\\' )
+testtweets = []
+for row in reader:
+    try:
+        testtweets.append( [row[0].encode('utf-8',"replace"), row[1] ])
+    except UnicodeDecodeError: 
+        pass
+for i in range(0, len(testtweets)):
+    X_test.append(testtweets[i][0])
+    Y_test.append(testtweets[i][1])
     
 X_test=np.array(X_test)
   
@@ -61,3 +70,11 @@ print wrong
 
 predictedlist=predicted.tolist()
 print nltk.ConfusionMatrix( Y_test, predictedlist )
+
+dic={'positive':0, 'neutral':1, 'negative':2}
+y_true=map(lambda x: dic[x], Y_test)
+y_predict=map(lambda x: dic[x], predictedlist)
+
+print(classification_report(y_true,y_predict,target_names=dic.keys()))
+
+print f1_score(Y_test, predictedlist, average='micro')
